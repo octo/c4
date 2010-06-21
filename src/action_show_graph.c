173727f52463a5e2f60e7363456f90905d67e669
Florian Forster <ff@octo.it>
2010-06-21 12:31:27 +0200

    "show graph" action: Implement displaying _graphs_ (rather than instances).
    A "breadcrump" field has been added, too.

--- src/action_show_graph.c
+++ src/action_show_graph.c
@@ -8,6 +8,7 @@
 
 #include "action_show_graph.h"
 #include "common.h"
+#include "graph_ident.h"
 #include "graph_list.h"
 #include "utils_cgi.h"
 
@@ -26,6 +27,54 @@ struct show_graph_data_s
   graph_instance_t *inst;
 };
 typedef struct show_graph_data_s show_graph_data_t;
+
+static void show_breadcrump_field (const char *str) /* {{{ */
+{
+  if ((str == NULL) || (str[0] == 0))
+    printf ("<em>none</em>");
+  else if (IS_ANY (str))
+    printf ("<em>any</em>");
+  else if (IS_ALL (str))
+    printf ("<em>all</em>");
+  else
+  {
+    char *str_html = html_escape (str);
+    printf ("<a href=\"%s?action=list_graphs;search=%s\">%s</a>",
+        script_name (), str_html, str_html);
+    free (str_html);
+  }
+} /* }}} void show_breadcrump_field */
+
+static int show_breadcrump (show_graph_data_t *data) /* {{{ */
+{
+  graph_ident_t *ident;
+  char *prefix;
+
+  if (data->inst != NULL)
+  {
+    prefix = "Instance";
+    ident = inst_get_selector (data->inst);
+  }
+  else
+  {
+    prefix = "Graph";
+    ident = graph_get_selector (data->cfg);
+  }
+
+  printf ("<div class=\"breadcrump\">%s: &quot;", prefix);
+  show_breadcrump_field (ident_get_host (ident));
+  printf ("&nbsp;/ ");
+  show_breadcrump_field (ident_get_plugin (ident));
+  printf ("&nbsp;&ndash; ");
+  show_breadcrump_field (ident_get_plugin_instance (ident));
+  printf ("&nbsp;/ ");
+  show_breadcrump_field (ident_get_type (ident));
+  printf ("&nbsp;&ndash; ");
+  show_breadcrump_field (ident_get_type_instance (ident));
+  printf ("&quot;</div>\n");
+
+  return (0);
+} /* }}} int show_breadcrump */
 
 static int show_instance_list_cb (graph_instance_t *inst, /* {{{ */
     void *user_data)
@@ -48,7 +97,8 @@ static int show_instance_list_cb (graph_instance_t *inst, /* {{{ */
   inst_get_params (data->cfg, inst, params, sizeof (params));
   html_escape_buffer (params, sizeof (params));
 
-  printf ("    <li class=\"instance\"><a href=\"%s?action=show_graph;%s\">%s</a></li>\n",
+  printf ("    <li class=\"instance\">"
+      "<a href=\"%s?action=show_graph;%s\">%s</a></li>\n",
       script_name (), params, descr);
 
   return (0);
@@ -57,19 +107,23 @@ static int show_instance_list_cb (graph_instance_t *inst, /* {{{ */
 static int show_instance_list (void *user_data) /* {{{ */
 {
   show_graph_data_t *data = user_data;
-  graph_instance_t *inst;
   char title[128];
+  char params[1024];
 
   memset (title, 0, sizeof (title));
   graph_get_title (data->cfg, title, sizeof (title));
   html_escape_buffer (title, sizeof (title));
 
-  printf ("<ul class=\"graph_list\">\n"
-      "  <li class=\"graph\">%s\n"
-      "  <ul class=\"instance_list\">\n", title);
+  memset (params, 0, sizeof (params));
+  graph_get_params (data->cfg, params, sizeof (params));
+  html_escape_buffer (params, sizeof (params));
 
-  inst = graph_get_instances (data->cfg);
-  inst_foreach (inst, show_instance_list_cb, user_data);
+  printf ("<ul class=\"graph_list\">\n"
+      "  <li class=\"graph\"><a href=\"%s?action=show_graph;%s\">%s</a>\n"
+      "  <ul class=\"instance_list\">\n",
+      script_name (), params, title);
+
+  graph_inst_foreach (data->cfg, show_instance_list_cb, user_data);
 
   printf ("  </ul>\n"
       "</ul>\n");
@@ -77,12 +131,14 @@ static int show_instance_list (void *user_data) /* {{{ */
   return (0);
 } /* }}} int show_instance_list */
 
-static int show_graph (void *user_data) /* {{{ */
+static int show_instance (void *user_data) /* {{{ */
 {
   show_graph_data_t *data = user_data;
   char title[128];
   char descr[128];
   char params[1024];
+
+  show_breadcrump (data);
 
   memset (title, 0, sizeof (title));
   graph_get_title (data->cfg, title, sizeof (title));
@@ -101,6 +157,14 @@ static int show_graph (void *user_data) /* {{{ */
       script_name (), params, title, descr);
 
   return (0);
+} /* }}} int show_instance */
+
+static int show_graph (void *user_data)
+{
+  show_graph_data_t *data = user_data;
+
+  show_breadcrump (data);
+  return (show_instance_list (user_data));
 } /* }}} int show_graph */
 
 int action_show_graph (void) /* {{{ */
@@ -109,32 +173,40 @@ int action_show_graph (void) /* {{{ */
   show_graph_data_t pg_data;
 
   char title[128];
-  char descr[128];
-  char html_title[128];
 
   pg_data.cfg = gl_graph_get_selected ();
   if (pg_data.cfg == NULL)
     OUTPUT_ERROR ("gl_graph_get_selected () failed.\n");
 
-  pg_data.inst = inst_get_selected (pg_data.cfg);
-  if (pg_data.inst == NULL)
-    OUTPUT_ERROR ("inst_get_selected (%p) failed.\n", (void *) pg_data.cfg);
-
   memset (title, 0, sizeof (title));
   graph_get_title (pg_data.cfg, title, sizeof (title));
 
-  memset (descr, 0, sizeof (descr));
-  inst_describe (pg_data.cfg, pg_data.inst, descr, sizeof (descr));
+  pg_data.inst = inst_get_selected (pg_data.cfg);
+  if (pg_data.inst != NULL)
+  {
+    char descr[128];
+    char html_title[128];
 
-  snprintf (html_title, sizeof (html_title), "Graph \"%s / %s\"",
-      title, descr);
-  html_title[sizeof (html_title) - 1] = 0;
+    memset (descr, 0, sizeof (descr));
+    inst_describe (pg_data.cfg, pg_data.inst, descr, sizeof (descr));
 
-  pg_callbacks.top_right = html_print_search_box;
-  pg_callbacks.middle_center = show_graph;
-  pg_callbacks.middle_left = show_instance_list;
+    snprintf (html_title, sizeof (html_title), "Graph \"%s / %s\"",
+        title, descr);
+    html_title[sizeof (html_title) - 1] = 0;
 
-  html_print_page (html_title, &pg_callbacks, &pg_data);
+    pg_callbacks.top_right = html_print_search_box;
+    pg_callbacks.middle_center = show_instance;
+    pg_callbacks.middle_left = show_instance_list;
+
+    html_print_page (html_title, &pg_callbacks, &pg_data);
+  }
+  else /* if (pg_data.inst == NULL) */
+  {
+    pg_callbacks.top_right = html_print_search_box;
+    pg_callbacks.middle_center = show_graph;
+
+    html_print_page (title, &pg_callbacks, &pg_data);
+  }
 
   return (0);
 } /* }}} int action_graph */
